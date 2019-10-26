e1b038432db99aaba5e7dc89a5ac4def59619ee2
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-10-26 18:22:11 -0300

    Add repo pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +28,35 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-github-api`,
+      options: {
+        // token: el que generamos recién, requerido por la API de GitHub
+        token: process.env.GATSBY_GH_KEY,
+
+        // GraphQLquery: con esta query, traemos a nuestro backend lo que
+        // queremos tener disponible para mostrar en nuestro frontend
+        graphQLQuery: `query {
+          user (login:"Guusy"){
+          repositories(first:50,orderBy:{field:STARGAZERS, direction:DESC},privacy:PUBLIC){
+            edges {
+              node {
+                id
+                name
+                description
+                isPrivate
+                url
+                }
+              }
+            }
+          }
+        }`,
+
+        // variables: por si necesitamos utilizar variables en nuestra query,
+        // no vamos a usarlo así que podemos borrarlo
+        //variables: algunObject,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
